454ba64453583a318ddb64af31946d5f0cbc6b49
Vanderson <vandersoncurty.contato@proton.me>
2026-01-15 14:51:58 -0300

    commit 2 base

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,109 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_deve_retornar_hello_world():
-    client = TestClient(app)
+def test_root_deve_retornar_hello_world(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, World!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password': 'securepassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'johndoe',
+        'email': 'johndoe@example.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'johndoe',
+                'email': 'johndoe@example.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1/',
+        json={
+            'username': 'janedoe',
+            'email': 'janedoe@example.com',
+            'password': 'securepassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'janedoe',
+        'email': 'janedoe@example.com',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1/')
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'id': 1,
+        'username': 'janedoe',
+        'email': 'janedoe@example.com',
+    }
+
+
+def test_update_error_user_not_found(client):
+    response = client.put(
+        '/users/999/',
+        json={
+            'username': 'nonexistent',
+            'email': 'nonexistent@example.com',
+            'password': 'securepassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_error_user_not_found(client):
+    response = client.delete('/users/999/')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_read_error_user_not_found(client):
+    response = client.get('/users/999/')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_read_user(client):
+    client.post(
+        '/users/',
+        json={
+            'username': 'johndoe',
+            'email': 'johndoe@example.com',
+            'password': 'securepassword',
+        },
+    )
+    response = client.get('/users/1/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'johndoe',
+        'email': 'johndoe@example.com',
+    }
---
+++ tests/test_db.py
@@ -0,0 +1,25 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='testuser', email='test@example.com', password='testpassword'
+        )
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'testuser'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'test@example.com',
+        'password': 'testpassword',
+        'created_at': time,
+    }
